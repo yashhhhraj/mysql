2159113a8d5f2bada9b4f630a54d2a1762a61356
Yashraj Singh Chouhan <yashraj21999@gmail.com>
2023-12-05 01:34:23 +0530

    2nd lesson done

---
+++ 1/insert_into_tables.sql
@@ -0,0 +1,19 @@
+create table phoness	(
+    name varchar(50),
+    price int not null
+);
+
+insert into phones
+values ( "AB" , 0);
+
+select * from phones;
+
+desc phones;
+
+create table phone4	(
+	id int primary key not null auto_increment,
+    name varchar(50),
+    price int not null
+);
+
+desc phone4
